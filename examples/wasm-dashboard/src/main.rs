a3b8fa5bb079d0124678e723084ff3950fcd5a42
randymcmillan <randymcmillan@protonmail.com>
2026-09-04 22:01:15 -0400

    examples/wasm-dashboard/src/main.rs

--- examples/wasm-dashboard/src/main.rs
+++ examples/wasm-dashboard/src/main.rs
@@ -183,9 +183,13 @@ fn main() -> io::Result<()> {
             "2" => info_keys.borrow_mut().tab = 1,
             "3" => info_keys.borrow_mut().tab = 2,
             "4" => info_keys.borrow_mut().tab = 3,
-            "Tab" => {
+            "ArrowRight" | "Tab" => {
                 let mut i = info_keys.borrow_mut();
                 i.tab = (i.tab + 1) % 4;
+            }
+            "ArrowLeft" => {
+                let mut i = info_keys.borrow_mut();
+                i.tab = (i.tab + 3) % 4;
             }
             "t" | "T" => {
                 let new_theme = info_keys.borrow().theme.toggle();
